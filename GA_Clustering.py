95e077f9702233c715cc190584e4132367ca546e
mowgliluciano <radisa05mitrovic.net@gmail.com>
2021-08-09 03:37:24 +0200

    Dodata funkcija za racunanje euklidskog rastojanja

--- GA_Clustering.py
+++ GA_Clustering.py
@@ -1,12 +1,18 @@
 import numpy as np
+from numpy.linalg import norm
+
+def euclidean_distances(X, Y):
+    X_Y = np.subtract(X,Y) # X - Y
+    return norm(X_Y)
+
 
 class GACluster:
     # ctor 
     def __init__(self, n_clusters=8, max_iter = 500, population_size = 100):
         self.n_clusters_ = n_clusters
         self.max_iter_ = max_iter
-        self.cluster_centers_ = [];
-        self.labels_ = []
+        self.cluster_centers_ = np.ndarray(0)
+        self.labels_ = np.ndarray(0)
         self.population_size_ = population_size
     
     # cluster_centers_ :ndarray of shape (n_clusters, n_features)
@@ -21,4 +27,9 @@ class GACluster:
     # inertia_ : float
     # Sum of squared distances of samples to their closest cluster center.
     def sse(self):
+        pass
+    
+    #X : ndarray 
+    #Compute k-means clustering
+    def fit(X):
         pass
