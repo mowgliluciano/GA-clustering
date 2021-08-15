3e2419f602ab5faf77a4dd25dcb6bbd9f9b8d51c
Milica153 <nina.mari29@gmail.com>
2021-08-15 17:31:43 +0200

    Dodata selekcija, ukrstanje i mutacija

--- ga.py
+++ ga.py
@@ -47,20 +47,103 @@ class Individual:
         for i in range(0,self.num_clasters):
             self.code[i] =  np.sum(clusters[i], axis=0)  / len(clusters[i])
             # print(f"Klaster {i} : {nove_centroide[i]}, suma = {suma}, novi centar = { suma / len(nove_centroide[i])}")
+            
+            
+def selection(population):
+    lengthOfPopulation = len(population)
+
+    calcFitness = np.zeros(lengthOfPopulation)
+    sumOfFitness = 0
+   # probabilities = np.zeros(lengthOfPopulation)
+
+    for i in range(lengthOfPopulation):
+        calcFitness[i] = population[i].fitness()
+        sumOfFitness += calcFitness[i]
+
+    #for i in range(lengthOfPopulation):
+    #    probabilities[i] = calcFitness[i] / sumOfFitness
+    #    print(probabilities[i], i)
+
+    indexes = sorted(list(zip(range(lengthOfPopulation), calcFitness)), key=lambda x: x[1])
+    #print(indexes)
+    k = 0
+    prob = random.random()
+    #print(prob)
+    for j in range(0, lengthOfPopulation):
+
+        if indexes[j][1] > prob:
+            break
+
+        #print(j)
+        k = indexes[j][0]
+        #print(k)
+
+    return k
+
+def crossover(parent1, parent2):
+    chromosomeLength = len(parent1.code)
+
+    child1 = parent1
+    child2 = parent2
+
+    i = random.randrange(chromosomeLength)
+
+    for j in range(i):
+        child1.code[j] = parent1.code[j]
+        child2.code[j] = parent2.code[j]
+
+    for j in range(i, chromosomeLength):
+        child1.code[j] = parent2.code[j]
+        child2.code[j] = parent1.code[j]
+
+    return child1, child2
+
+
+def mutation(individual, number):
+    chromosomeLength = len(individual.code)
+    gene = len(individual.code[0])
+    sign = -1
+    if (random.random() < 0.5):
+        sign = 1
+
+    for i in range(chromosomeLength):
+        r = random.random()
+        print(r)
+        if r > number:
+            continue
+        delta = random.uniform(0, 1)
+        for j in range(gene):
+            if (individual.code[i][j] == 0):
+                individual.code[i][j] = sign * 2 * delta
+        individual.code[i][j] += sign * 2 * delta * individual.code[i][j]
+
+    return individual
+
 
 
 def genethic_algorithm(num_clasters, points, mutation_rate, pop_size, max_iter, elitism_size):
     #kreiranje inicijalne populacije
     population = [Individual(num_clasters,points, mutation_rate,firstInit=True) for _ in range(pop_size)]
+    newPopulation = [Individual(num_clasters, points, mutation_rate, firstInit=True) for _ in range(pop_size)]
     #izracunavanje fitnesa za svaku jedinku -> neophodno je da isklasterujem tacke i kreiram nove centroide
     for individual in population:
         individual.precomputeDistances()
         # print(individual.fitness())
 
-    # for i in range(max_iter):
-        #selekcija...
-        #ukrstanje...
-        #mutacija...
+    for i in range(pop_size):
+        newPopulation[i] = population[i]
+
+    for i in range(max_iter):
+        for j in range(pop_size-1):
+            k1 = selection(population)
+            k2 = selection(population)
+            newPopulation[j], newPopulation[j+1] = crossover(population[k1], population[k2])
+            #mutation(newPopulation[j], mutation_rate)
+        # mutacija...
+        # elitizam...
+        # smena generacija...
+
+        population = newPopulation
         #elitizam...
         #smena generacija...
     return population[0]
